630ce8d3586c4334b9ecd96f75186cdf22bdbf76
nitziacovg <nitzia.covarrubias@icloud.com>
2026-07-18 00:10:35 -0400

    feat: adding car models, populate data and path to url.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,8 +1,7 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -15,4 +14,9 @@ urlpatterns = [
 
     # path for add a review view
 
+    # path for get cars
+    path(route='get_cars', view=views.get_cars, name='getcars'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
