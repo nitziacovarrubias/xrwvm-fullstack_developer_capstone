bad0318bff96264f0cf6ba8663d7904ea4aec0ef
nitziacovg <nitzia.covarrubias@icloud.com>
2026-07-20 13:51:06 -0400

    feat: sentiment analyzer for dealer reviews

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,8 +11,12 @@ urlpatterns = [
     # path(route='login', view=views.login_user, name='login'),
 
     # path for dealer reviews view
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
+    path(route='add_review', view=views.add_review, name='add_review'),
 
     # path for get cars
     path(route='get_cars', view=views.get_cars, name='getcars'),
